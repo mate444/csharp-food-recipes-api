38d44d8cb5333442d9addc514af87197ec8f6d7c
mate444 <mateoeo23@gmail.com>
2023-08-01 18:15:13 +0200

    Adds Repository Interface

---
+++ food-recipes-api/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace food_recipes_api.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(Guid Id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+    }
+}
