461b2145acbb0b358efb70e0f230169a15507451
mate444 <mateoeo23@gmail.com>
2023-08-01 19:14:51 +0200

    Adds Generic Repository

--- food-recipes-api/Repositories/IRepository.cs
+++ food-recipes-api/Repositories/IRepository.cs
@@ -4,7 +4,7 @@ namespace food_recipes_api.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
-        TEntity Get(Guid Id);
+        TEntity? Get(Guid Id);
         IEnumerable<TEntity> GetAll();
         IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
 
---
+++ food-recipes-api/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace food_recipes_api.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+        public TEntity? Get(Guid id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().AddRange(entities);
+        }
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+    }
+}
