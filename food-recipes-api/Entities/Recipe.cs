eaaf1700ee9851ce2b4a0d9cf175d16b29b295f1
mate444 <mateoeo23@gmail.com>
2023-12-04 15:15:28 +0100

    Fixes error CS8618

--- food-recipes-api/Entities/Recipe.cs
+++ food-recipes-api/Entities/Recipe.cs
@@ -4,13 +4,11 @@
     {
         public Guid Id { get; set; }
 
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Instructions { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Instructions { get; set; } = string.Empty;
         public int Score { get; set; }
         public int HealthScore { get; set; }
-        public string Image { get; set; }
-        public string Summary { get; set; }
-
+        public string Image { get; set; } = string.Empty;
     }
 }
